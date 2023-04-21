88d969b1ef136f36c42087edb090100507d1503f
Saumya Patil <saumyasachinpatil@gmail.com>
2023-04-21 16:21:31 +0530

    Seperate the duration changer view into functions
    The entire view was a single 100+ line function, so I ended up seperating
    it into 3 functions: a click event handler, a input element, and a public
    one that joins the two together.
    
    Also, why does any variable only used inside a macro show up as "unused"
    by rust analyzer? This is annoying, as I need to prefix a lot of variables
    with an underscore so that I don't get a ton of warnings.

--- src/views/timer/duration_changer.rs
+++ src/views/timer/duration_changer.rs
@@ -1,19 +1,19 @@
 //! This module contains the view `TimerDurationChanger`.
-//! 
+//!
 //! This view allows the user to change the duration of the timer.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! Inside a view:
 //! ```html
 //! <TimerDurationChanger timer=timer set_timer=timer/>
 //! ```
-//! 
-//! 
+//!
+//!
 
 use chrono::Duration;
 
-use leptos::*;
+use leptos::{ev::Event, *};
 use tracing::info;
 
 use crate::timer::Timer;
@@ -31,79 +31,114 @@ pub fn TimerDurationChanger(
     timer: ReadSignal<Timer>,
     set_timer: WriteSignal<Timer>,
 ) -> impl IntoView {
-    let (_a, set_a) = create_signal(cx, timer.get().total_time.to_string());
-    let on_input = move |event| {
-        let duration_string = event_target_value(&event);
-
-        info!("Changing timer duration to: {}", duration_string);
-
-        let duration = duration_string
-            .parse::<i32>()
-            .expect("duration to be parsable to i32");
-
-        set_a(duration_string);
-
-        let duration = Duration::minutes(duration as i64);
-
-        if duration != timer.get().total_time {
-            info!("Setting timer to new duration: {}", duration);
-            set_timer.update(|timer| {
-                *timer = Timer::new(duration);
-            });
-        } else {
-            info!("Duration is the same as the current duration. Doing nothing.");
-        }
-    };
-    
+    let (_current_selection, set_current_selection) =
+        create_signal(cx, timer.get().total_time.to_string());
+    let on_input = move |event| click_event_handler(event, timer, set_timer, set_current_selection);
 
     view! {
         cx,
         <div>
             <h3>"Change Timer Duration: "</h3>
-
-            // TODO: Seperate into a different component.
-            <input 
-                type="radio"
-                value="30" 
-                name="timer-duration-changer" 
-                id="work-radio-button" 
-                on:input=on_input
-                checked
-            />
-            <label 
-                for="work-radio-button"
-            >
-                "Work"
-            </label>
-
-            
-            <input 
-                type="radio" 
-                value="5" 
-                name="timer-duration-changer" 
-                id="short-break-radio-button" 
-                on:input=on_input
+ 
+            <InputElement 
+                _value="30"
+                _id="work-radio-button"
+                _on_input=on_input
+                _label="Work"
+                _checked=true
             />
 
-            <label 
-                for="short-break-radio-button"
-            >
-                "Short Break"
-            </label>
-
-            <input 
-                type="radio" 
-                value="15" 
-                name="timer-duration-changer" 
-                id="long-break-radio-button" 
-                on:input=on_input
+            <InputElement 
+                _value="5"
+                _id="short-break-radio-button"
+                _on_input=on_input
+                _label="Short Break"
+                _checked=false
             />
 
-            <label 
-                for="long-break-radio-button"
-            >
-                "Long Break"
-            </label>
+            <InputElement 
+                _value="15"
+                _id="long-break-radio-button"
+                _on_input=on_input
+                _label="Long Break"
+                _checked=false
+            />
         </div>
+    }
+}
+
+/// Displays a radio button and a label.
+/// and sets a click event handler for the radio button.
+/// 
+/// # Arguments
+/// 
+/// * `_value` - The value of the radio button.
+/// * `_id` - The id of the radio button.
+/// * `_on_input` - The event handler for the radio button.
+/// * `_label` - The label for the radio button.
+/// * `_checked` - Whether or not the radio button is checked.
+#[component]
+fn input_element<T>(
+    cx: Scope,
+    _value: &'static str,
+    _id: &'static str,
+    _on_input: T,
+    _label: &'static str,
+    _checked: bool,
+) -> impl IntoView
+where
+    T: FnMut(leptos::ev::Event) -> () + 'static,
+{
+    view! {
+        cx,
+        <input
+            type="radio"
+            value=_value
+            name="timer-duration-changer"
+            id=_id
+            on:input=_on_input
+            checked=_checked
+        />
+        <label
+            for=_id
+        >
+            { _label }
+        </label>
+    }
+}
+
+/// Handles the click event for the `TimerDurationChanger` component.
+///
+/// # Arguments
+///
+/// * `event` - The event that triggered this function.
+/// * `timer` - The getter for the timer.
+/// * `set_timer` - The setter for the timer.
+/// * `set_current_selection` - The setter for the current selection.
+fn click_event_handler(
+    event: Event,
+    timer: ReadSignal<Timer>,
+    set_timer: WriteSignal<Timer>,
+    set_current_selection: WriteSignal<String>,
+) {
+    let duration_string = event_target_value(&event);
+
+    info!("Changing timer duration to: {}", duration_string);
+
+    let duration = duration_string
+        .parse::<i32>()
+        .expect("duration to be parsable to i32");
+
+    set_current_selection(duration_string);
+
+    let duration = Duration::minutes(duration as i64);
+
+    if duration != timer.get().total_time {
+        info!("Setting timer to new duration: {}", duration);
+        set_timer.update(|timer| {
+            *timer = Timer::new(duration);
+        });
+    } else {
+        info!("Duration is the same as the current duration. Doing nothing.");
     }
 }
